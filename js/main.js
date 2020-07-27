e9c4de8354f8c07abdcb68f0275bb7b30e23652c
Arthur Plyuschevsky <62159695+massqeen@users.noreply.github.com>
2020-07-27 15:25:16 +0300

    added day1 code

---
+++ js/main.js
@@ -0,0 +1,29 @@
+import { radioPlayerInit } from './radioplayer.js';
+import { videoPlayerInit } from './videoplayer.js';
+import { musicPlayerInit } from './musicplayer.js';
+
+const playerBlock = document.querySelectorAll('.player-block'),
+  playerBtn = document.querySelectorAll('.player-btn');
+
+const deactivateTempHeader = () =>
+  (document.querySelector('.temp').style.display = 'none');
+
+const deactivateTabs = () => {
+  playerBlock.forEach((item) => item.classList.remove('active'));
+  playerBtn.forEach((item) => item.classList.remove('active'));
+};
+
+const playerBtnClickHandler = (btn, i) => {
+  deactivateTabs();
+  deactivateTempHeader();
+  btn.classList.add('active');
+  playerBlock[i].classList.add('active');
+};
+
+playerBtn.forEach((btn, i) => {
+  btn.addEventListener('click', () => playerBtnClickHandler(btn, i));
+});
+
+radioPlayerInit();
+videoPlayerInit();
+musicPlayerInit();
