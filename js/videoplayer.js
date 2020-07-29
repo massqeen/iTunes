ab781d00808f2eb805bede1d9dd4eec58f0c48de
massqeen <62159695+massqeen@users.noreply.github.com>
2020-07-29 17:31:57 +0300

    added fullscreen and volume controls for videoplayer

--- js/videoplayer.js
+++ js/videoplayer.js
@@ -1,10 +1,15 @@
 export const videoPlayerInit = () => {
   const videoBtnPlay = document.querySelector('.video-button__play'),
     videoBtnStop = document.querySelector('.video-button__stop'),
+    videoFullscreen = document.querySelector('.video-fullscreen'),
     videoPLayer = document.querySelector('.video-player'),
     videoProgress = document.querySelector('.video-progress'),
     videoTimePassed = document.querySelector('.video-time__passed'),
-    videoTimeTotal = document.querySelector('.video-time__total');
+    videoTimeTotal = document.querySelector('.video-time__total'),
+    videoVolume = document.querySelector('.video-volume'),
+    videoVolumeIcon = document.querySelector('.video-volume-icon');
+
+  videoPLayer.volume = videoVolume.value / 100;
 
   const videoClickHandler = () => {
     if (videoPLayer.paused) {
@@ -22,6 +27,10 @@ export const videoPlayerInit = () => {
       videoBtnPlay.classList.add('fa-pause');
       videoBtnPlay.classList.remove('fa-play');
     }
+  };
+
+  const enterFullScreen = () => {
+    videoPLayer.requestFullscreen();
   };
 
   const stopClickHandler = () => {
@@ -46,11 +55,30 @@ export const videoPlayerInit = () => {
 
   const videoProgressUpdate = () => videoPLayer.currentTime = videoProgress.value * videoPLayer.duration / 100;
 
+  const videoVolumeUpdate = () => {
+    videoPLayer.volume = videoVolume.value / 100;
+  };
+
+  const videoVolumeClickHandler = () => {
+    if (videoPLayer.volume) {
+      videoPLayer.volume = 0;
+      videoVolumeIcon.classList.remove('fa-volume-up');
+      videoVolumeIcon.classList.add('fa-volume-off');
+      return;
+    }
+    videoPLayer.volume = videoVolume.value / 100;
+    videoVolumeIcon.classList.add('fa-volume-up');
+    videoVolumeIcon.classList.remove('fa-volume-off');
+  };
+
   videoBtnPlay.addEventListener('click', videoClickHandler);
   videoBtnStop.addEventListener('click', stopClickHandler);
+  videoFullscreen.addEventListener('click', enterFullScreen);
   videoPLayer.addEventListener('click', videoClickHandler);
   videoPLayer.addEventListener('play', toggleIcon);
   videoPLayer.addEventListener('pause', toggleIcon);
   videoPLayer.addEventListener('timeupdate', timeUpdate);
-  videoProgress.addEventListener('change', videoProgressUpdate);
+  videoProgress.addEventListener('input', videoProgressUpdate);
+  videoVolume.addEventListener('input', videoVolumeUpdate);
+  videoVolumeIcon.addEventListener('click', videoVolumeClickHandler);
 };
